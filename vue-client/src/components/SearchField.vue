994b54cea529ee659cf2d7718fd94222bceddf73
Draklaw <drakdiv@gmail.com>
2019-12-01 00:57:54 +0100

    Add search field for collections.

---
+++ vue-client/src/components/SearchField.vue
@@ -0,0 +1,20 @@
+<template>
+	<input
+		v-model="value"
+		v-on:input="$emit('input', $event.target.value)"
+		placeholder="Search"
+	/>
+</template>
+
+
+<script>
+export default {
+	data() {
+		return {
+		}
+	},
+	props: {
+		value: String,
+	},
+}
+</script>
--- vue-client/src/views/Collection.vue
+++ vue-client/src/views/Collection.vue
@@ -2,9 +2,10 @@
 	<div>
 		<template v-if="loaded">
 			<h2>{{ collection.title }}</h2>
+			<search-field v-model="searchQuery" />
 			<collection-table
 				v-if="loaded"
-				v-bind:items="collection.items"
+				v-bind:items="filteredItems"
 				v-bind:fields="collection.fields"
 			/>
 		</template>
@@ -17,17 +18,20 @@ import store from '@/store/store.js'
 
 import Loading from '@/components/Loading.vue'
 import CollectionTable from '@/components/CollectionTable.vue'
+import SearchField from '@/components/SearchField.vue'
 
 export default {
 	name: 'collection',
 	components: {
 		Loading,
 		CollectionTable,
+		SearchField,
 	},
 	data() {
 		return {
 			store,
 			loaded: false,
+			searchQuery: "",
 		}
 	},
 	computed: {
@@ -40,6 +44,18 @@ export default {
 		collection() {
 			return this.store.getCollection(this.username, this.collectionName)
 		},
+		filteredItems() {
+			if(this.searchQuery === "")
+				return this.collection.items
+
+			const query = this.searchQuery.toLowerCase()
+			return this.collection.items.filter(
+				item => {
+					const title = item.title.toLowerCase()
+					return title.includes(query)
+				}
+			)
+		}
 	},
 	methods:{
 		async update() {
